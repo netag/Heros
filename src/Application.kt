fe5b49141b1d3360faf2ab1f535db1506eb7cde4
Neta Globerman <ngloberman@newrelic.com>
2020-04-13 15:23:21 +0300

    feat(ktor-serialization)

--- src/Application.kt
+++ src/Application.kt
@@ -1,14 +1,24 @@
 package com.netag
 
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
-import io.ktor.features.*
-import io.ktor.routing.*
-import io.ktor.http.*
-import io.ktor.auth.*
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.auth.Authentication
+import io.ktor.features.CallLogging
+import io.ktor.features.ContentNegotiation
+import io.ktor.features.DefaultHeaders
+import io.ktor.http.ContentType
+import io.ktor.response.respond
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.serialization.json
+import kotlinx.serialization.Serializable
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Serializable
+data class MyResponse(val id: Int, val name: String)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
@@ -17,15 +27,18 @@ fun Application.module(testing: Boolean = false) {
         header("X-Engine", "Ktor") // will send this header with each response
     }
 
+    install(CallLogging)
+
     install(Authentication) {
     }
 
     install(ContentNegotiation) {
+        json()
     }
 
     routing {
         get("/") {
-            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+            call.respond(MyResponse(5, "neta"))
         }
     }
 }
