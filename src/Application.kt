3cf7dc0326c83880dcf71ed1f458a692595cc072
Neta Globerman <ngloberman@newrelic.com>
2020-04-13 18:20:58 +0300

    fix(graphql:query)

--- src/Application.kt
+++ src/Application.kt
@@ -1,18 +1,11 @@
 package com.netag
 
+import com.apurebase.kgraphql.graphql
 import io.ktor.application.Application
-import io.ktor.application.call
 import io.ktor.application.install
-import io.ktor.auth.Authentication
 import io.ktor.features.CallLogging
-import io.ktor.features.ContentNegotiation
 import io.ktor.features.DefaultHeaders
-import io.ktor.http.ContentType
-import io.ktor.response.respond
-import io.ktor.response.respondText
-import io.ktor.routing.get
 import io.ktor.routing.routing
-import io.ktor.serialization.json
 import kotlinx.serialization.Serializable
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
@@ -26,19 +19,15 @@ fun Application.module(testing: Boolean = false) {
     install(DefaultHeaders) {
         header("X-Engine", "Ktor") // will send this header with each response
     }
-
     install(CallLogging)
 
-    install(Authentication) {
-    }
-
-    install(ContentNegotiation) {
-        json()
-    }
-
     routing {
-        get("/") {
-            call.respond(MyResponse(5, "neta"))
+        graphql {
+            query("response") {
+                resolver { name: String ->
+                    MyResponse(4, name)
+                }
+            }
         }
     }
 }
